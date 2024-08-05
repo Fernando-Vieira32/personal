5383fe8d1163ac1598c05c4612ec1d28febe3589
Fernando Vieira <fernandovieira.32@hotmail.com>
2024-08-04 23:13:22 -0300

    (add_simplecov/a5e0hquj) shoulda-matchers config

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -5,8 +5,6 @@ module Users
     before_action :configure_sign_up_params, only: [:create]
     before_action :configure_account_update_params, only: [:update]
 
-  
-
     def configure_sign_up_params
       devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
     end
@@ -14,6 +12,5 @@ module Users
     def configure_account_update_params
       devise_parameter_sanitizer.permit(:account_update, keys: [:name])
     end
-
   end
 end
